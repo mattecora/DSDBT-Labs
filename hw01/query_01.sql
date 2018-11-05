c7d61e45c3425005c0974cf72d86b5d58bc01e48
Matteo Corain <m96.corain@gmail.com>
2018-11-05 17:14:29 +0100

    Hw #1: fixed inconsistencies, queries updated

---
+++ hw01/query_01.sql
@@ -0,0 +1,18 @@
+SELECT      PurchaseMode,
+            PurchaseMonth,
+            PurchaseYear,
+            ROUND(SUM(Revenues)/EXTRACT(
+                DAY FROM LAST_DAY(TO_DATE(PurchaseMonth, 'MM-YYYY')))
+                , 2) AS AvgDailyRevenue,
+            SUM(SUM(Revenues)) OVER (
+                PARTITION BY PurchaseYear
+                ORDER BY PurchaseMonth, PurchaseMode
+                ROWS UNBOUNDED PRECEDING
+                ) AS RevenueFromBeginningOfYear,
+            ROUND(SUM(NumberOfTickets)/SUM(SUM(NumberOfTickets))
+                OVER (PARTITION BY PurchaseMonth) * 100
+                , 2) AS PercentageOverMonth
+FROM        F_TICKETSALES TS, D_PURCHASEDATE PD
+WHERE       TS.PurchaseDate_ID = PD.PurchaseDate_ID
+GROUP BY    PurchaseMode, PurchaseMonth, PurchaseYear
+ORDER BY    PurchaseMonth, PurchaseMode;
---
+++ hw01/query_02.sql
@@ -0,0 +1,19 @@
+SELECT      SingerNationality,
+            EventCity,
+            EventProvince,
+            EventRegion,
+            ROUND(SUM(Revenues)/SUM(NumberOfTickets), 2) AS AvgTicketRevenue,
+            ROUND(SUM(Revenues)/SUM(SUM(Revenues))
+                OVER (PARTITION BY EventProvince) * 100
+                , 2) AS PercentageOverProvince,
+            ROUND(SUM(Revenues)/SUM(SUM(Revenues))
+                OVER (PARTITION BY EventRegion) * 100
+                , 2) AS PercentageOverRegion
+FROM        F_TICKETSALES TS, D_PURCHASEDATE PD,
+            D_EVENTDATE ED, D_SINGER S, D_EVENTLOCATION EL
+WHERE       TS.PurchaseDate_ID = PD.PurchaseDate_ID
+AND         TS.EventDate_ID = ED.EventDate_ID
+AND         TS.Singer_ID = S.Singer_ID
+AND         TS.EventLocation_ID = EL.EventLocation_ID
+AND         EventYear = 2017
+GROUP BY    SingerNationality, EventCity, EventProvince, EventRegion;
