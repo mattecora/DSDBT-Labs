b6bc6ef6bd4d0e7ed6fdc11f42f16ccf6dc1719f
Matteo Corain <m96.corain@gmail.com>
2018-11-10 12:43:10 +0100

    Hw #1: singer and tour hierarchies joint

--- hw01/query_02.sql
+++ hw01/query_02.sql
@@ -9,11 +9,11 @@ SELECT      SingerNationality,
             ROUND(SUM(Revenues)/SUM(SUM(Revenues))
                 OVER (PARTITION BY SingerNationality, EventRegion) * 100
                 , 2) AS PercentageOverRegion
-FROM        F_TICKETSALES TS, D_PURCHASEDATE PD,
-            D_EVENTDATE ED, D_SINGER S, D_EVENTLOCATION EL
+FROM        F_TICKETSALES TS, D_EVENTDATE ED,
+            D_TOUR T, D_EVENTLOCATION EL
 WHERE       TS.PurchaseDate_ID = PD.PurchaseDate_ID
 AND         TS.EventDate_ID = ED.EventDate_ID
-AND         TS.Singer_ID = S.Singer_ID
+AND         TS.Tour_ID = T.Tour_ID
 AND         TS.EventLocation_ID = EL.EventLocation_ID
 AND         EventYear = 2017
 GROUP BY    SingerNationality, EventCity, EventProvince, EventRegion
