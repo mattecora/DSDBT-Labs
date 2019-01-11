e39b7fab65c05687d402e2239109e681c200cbec
Matteo Corain <m96.corain@gmail.com>
2019-01-11 10:23:55 +0100

    Hw #4: added alternative tree, other minor updates

--- hw04/compile_trees.bat
+++ hw04/compile_trees.bat
@@ -3,3 +3,5 @@
 D:\Utilities\graphviz\bin\dot.exe -Gdpi=300 -Nfontname="Cambria Math" -Gfontname="Cambria Math" ex_tree_orig.gv -Tpng -o ex_tree_orig.png
 
 D:\Utilities\graphviz\bin\dot.exe -Gdpi=300 -Nfontname="Cambria Math" -Gfontname="Cambria Math" ex_tree_opt.gv -Tpng -o ex_tree_opt.png
+
+D:\Utilities\graphviz\bin\dot.exe -Gdpi=300 -Nfontname="Cambria Math" -Gfontname="Cambria Math" ex_tree_alt.gv -Tpng -o ex_tree_alt.png
